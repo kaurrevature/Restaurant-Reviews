6ec9ef0c1c5c567e5942e8ce8ad6b666c5f1a506
jkcherrelus <jkcherrelus6038@eagle.fgcu.edu>
2018-01-10 13:12:45 -0500

    init
    init

---
+++ YummyTummy/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YummyTummy.Models;
+
+namespace YummyTummy.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        Address address = new Address();
+        
+        public ActionResult Index()//Action Methods
+        {
+            ViewBag.Name = "Chik-fil-e";// C# 4.0
+            ViewData["Location"] = "Reston VA";
+            TempData["Country"] = "USA";
+            return File();
+        }
+        //public string Index()
+        //{
+        //    return "Hello World";
+        // }
+
+        public ContentResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return Content("This Application gives restuarants with best reviews");
+        }
+        public JsonResult JsonData()
+        {
+            address.Street = "Plaza America";
+            address.City = "Reston";
+            address.State = "VA";
+            address.Country = "USA";
+            address.ZipCode = "20190";
+            address.Phone = "XXX-XXX-XXXX";
+            address.Email = "wholefoods@restonwf.com";
+            return Json(address,JsonRequestBehavior.AllowGet);
+        }
+
+        public ViewResult Contact()
+        {
+            ViewBag.Message = "Contact Whole Foods";
+            address.Street = "Plaza America";
+            address.City = "Reston";
+            address.State = "VA";
+            address.Country = "USA";
+            address.ZipCode = "20190";
+            address.Phone = "XXX-XXX-XXXX";
+            address.Email = "wholefoods@restonwf.com";
+            return View(address);
+        }
+    }
+}
---
+++ YummyTummy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YummyTummy.Startup))]
+namespace YummyTummy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
